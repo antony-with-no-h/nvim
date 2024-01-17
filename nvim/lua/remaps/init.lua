c4e812f83c785b696ab8497ce5109dc98d6e1dcb
antony-with-no-h <94751539+antony-with-no-h@users.noreply.github.com>
2024-01-17 15:13:08 +0000

    add remaps

--- nvim/lua/remaps/init.lua
+++ nvim/lua/remaps/init.lua
@@ -1,1 +1,5 @@
 vim.keymap.set('n', '<leader>dw', 'bdw', {})
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
