ecb1da51284572d134f7ef06c4e424678b36e3e8
antony-with-no-h <94751539+antony-with-no-h@users.noreply.github.com>
2024-11-20 06:10:38 +0000

    typo; adjust cc

--- nvim/lua/options/init.lua
+++ nvim/lua/options/init.lua
@@ -1,12 +1,12 @@
 -- general text editing
 vim.o.tabstop = 4
-vim.o.softttabstop = 4
+vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
 
 -- ui
-vim.o.colorcolumn = 80
+vim.o.colorcolumn = "100"
 vim.o.signcolumn = "yes"
 vim.o.scrolloff = 8
 vim.o.relativenumber = true
